178bdadcb89705064c9d74b91b52f756bf661412
agataogorkis <agataogorkis@gmail.com>
2021-10-26 20:23:19 +0200

    zmiana hello world

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        System.out.printf("hello world");
-        
+        System.out.printf("hello poland");
+
     }
     private void displayText(){ 
         System.out.println("zmiana w drugim repo");
